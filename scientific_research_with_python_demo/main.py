7bb0b8672297620ce937f72849f8db302647332b
applepie1026 <1287499419@qq.com>
2023-02-15 18:23:45 +0800

    1.the prograss of parameters estimation by using best_index 2. request more information about ind2sub by python 3.tests about construct paramters search_space and maximum are done

--- scientific_research_with_python_demo/main.py
+++ scientific_research_with_python_demo/main.py
@@ -1,72 +1,88 @@
 import numpy as np
 
 WAVELENGTH = 0.0056  # [unit:m]
-def wrap_phase(phase:np.ndarray)->np.ndarray:
-    return (phase + np.pi) % (2 * np.pi) - np.pi# 缠绕相位,测试，测试,test
 
-def construct_simulated_arc_phase(dv:float, dh:float, noise_level:float)->np.ndarray:
+
+def wrap_phase(phase: np.ndarray) -> np.ndarray:
+    return (phase + np.pi) % (2 * np.pi) - np.pi
+
+
+def construct_simulated_arc_phase(dv: float, dh: float, noise_level: float) -> np.ndarray:
 
     dv_phase = v2phase(dv)
     dh_phase = h2phase(dh)
     noise_phase = generate_phase_noise(noise_level)*4*np.pi/WAVELENGTH
-    arc_phase=dv_phase + dh_phase + noise_phase
+    arc_phase = dv_phase + dh_phase + noise_phase
     return arc_phase
 
-def v2phase(v:float, time_range:np.ndarray=np.arange(20))->np.ndarray:
+
+def v2phase(v: float, time_range: np.ndarray = np.arange(20)) -> np.ndarray:
     """Calculate phase difference from velocity difference (of two points).
     """
     temporal_baseline = 12  # [unit:d]
-    Btemp=temporal_baseline*time_range
+    temporal_samples = temporal_baseline*time_range
     # distance = velocity * days (convert from d to yr because velocity is in m/yr)
-    distance = v * Btemp / 365   # [unit:m]
-    return distance * 4 * np.pi / WAVELENGTH  # [unit:rad]
+    distance = v * temporal_samples / 365   # [unit:m]
+    v2phase_coefficeint = 4 * np.pi * temporal_samples / WAVELENGTH*365
+    return v2phase_coefficeint*v, v2phase_coefficeint  # [unit:rad]
 
-def h2phase(h:float)->np.ndarray:
-    B_normal=300#[unit:m]
-    H=780000# satellite vertical height[m]
-    theta=23*np.pi/180
-    R=H/np.cos(theta)
-    Bn=4*np.pi*B_normal/(WAVELENGTH*R*np.sin(theta))
-    return Bn*h*4*np.pi/WAVELENGTH
 
-def generate_phase_noise(noise_level:float)->np.ndarray:
-    noise=np.random.normal(loc=0.0,scale=noise_level,size=(1,20))
+def h2phase(h: float) -> np.ndarray:
+    normal_baseline = 300  # [unit:m]
+    H = 780000  # satellite vertical height[m]
+    incidence_angle = 23*np.pi/180
+    R = H/np.cos(incidence_angle)
+    h2ph_coefficient = 4*np.pi*normal_baseline / \
+        WAVELENGTH*R*np.sin(incidence_angle)
+    return h2ph_coefficient*h, h2ph_coefficient
+
+
+def generate_phase_noise(noise_level: float) -> np.ndarray:
+    noise = np.random.normal(loc=0.0, scale=noise_level,
+                             size=(1, 20))*(4*np.pi/WAVELENGTH)
     return noise
 
-def construct_param_search_space(step:float,Nsearch,A_matrix)->np.ndarray:
+
+def construct_param_search_space(step: float, Nsearch, A_matrix) -> np.ndarray:
     """
     construct estimated parameters' search space
     such as height [-10:1:10]
     input: step  :step for search
-           2*Nsearch :number of paramters we can search   
+           2*Nsearch :number of paramters we can search
     创建参数搜索区域[-10:1:10]"""
-    Search_space=np.mat(np.arange(-Nsearch*step,Nsearch*step,step))
-    Search_space_phase=np.dot(A_matrix,Search_space)
+    Search_space = np.mat(np.arange(-Nsearch*step, Nsearch*step, step))
+    Search_space_phase = np.dot(A_matrix, Search_space)
     return Search_space_phase
 
-def construct_search_space(phase_search1,phase_serach2,num_serach)->np.ndarray:
-    search_space=np.kron(phase_search1,np.ones(1,num_serach[1]))+np.kron(np.ones(1,num_serach[0]),phase_serach2) 
+
+def construct_search_space(phase_search1, phase_serach2, num_serach) -> np.ndarray:
+    # the condition that we have two variables (h,v)
+    # 把v和h对应的phase_model展开成相同维度均为（Nifgs*v_search_number*h_search_number）
+    search_space = np.kron(phase_search1, np.ones(
+        1, num_serach[1]))+np.kron(np.ones(1, num_serach[0]), phase_serach2)
     return search_space
 
-def maximum_main(dphase,search_space,row_num_serach,num_search,parm,Nsearch):
-    coh_phase=dphase*np.ones((1,row_num_serach))-search_space
-    coh_t=np.sum(np.exp(1j*coh_phase),axis= 0)
-    best=np.max(coh_t)
-    best_index=np.argmax(coh_t,axis=1)
-    #caculate parameters
-    a=np.unravel_index(best_index,(num_search[1],num_search[0]),order="C")
-    parm=[parm[0]+(a[0]-(Nsearch[0]+1)),parm[1]+(a[1]-(Nsearch[1]+1))]
-    return best,parm
+
+def maximum_coh_temporal(dphase, search_space, row_num_serach, num_search, parm, Nsearch, step):
+    # coh_phase=phase_observation-phase_model
+    coh_phase = dphase*np.ones((1, row_num_serach))-search_space
+    coh_t = np.sum(np.exp(1j*coh_phase), axis=0)
+    best = np.max(coh_t)
+    best_index = np.argmax(coh_t, axis=1)
+    # caculate parameters
+    a = np.unravel_index(best_index, (num_search[1], num_search[0]), order="C")
+    parm = [parm[0]+(a[0]-(Nsearch[0]))*step[0],
+            parm[1]+(a[1]-(Nsearch[1]))*step[1]]
+    return best, parm, a, best_index
 
 
-
-v_orig=0.01
-h_orig=[10]*20+np.random.normal(loc=0,scale=1,size=(1,20))
-noise_level=0.1
-phase_unwrapped=construct_simulated_arc_phase(v_orig,h_orig,noise_level).T
-print(h_orig)
-print(h2phase(h_orig))
-print(wrap_phase(phase_unwrapped))
+# v_orig = 0.01
+# h_orig = [10]*20+np.random.normal(loc=0, scale=1, size=(1, 20))
+# noise_level = 0.1
+# phase_unwrapped = construct_simulated_arc_phase(v_orig, h_orig, noise_level).T
+# print(h_orig)
+# print(h2phase(h_orig))
+# print(wrap_phase(phase_unwrapped))
 # print(type(generate_phase_noise(0.1)))
 # def estimate_parameters(constructed_simulated_phase):
 #     # TODO: implement this function
--- test/test_main.py
+++ test/test_main.py
@@ -1,12 +1,12 @@
 import pytest
 import numpy as np
-from scientific_research_with_python_demo.main import v2phase,h2phase,generate_phase_noise,construct_simulated_arc_phase,construct_param_search_space,maximum_main,WAVELENGTH
+from scientific_research_with_python_demo.main import v2phase, h2phase, generate_phase_noise, construct_simulated_arc_phase, construct_param_search_space, maximum_coh_temporal, WAVELENGTH
 
 
 def test_v2phase():
     # construct a simulated case
-    simuated_v = 0.1 * WAVELENGTH # [unit:m/yr]
-    simuated_time_range = np.array([0,1,2]) * 365 / 12
+    simuated_v = 0.1 * WAVELENGTH  # [unit:m/yr]
+    simuated_time_range = np.array([0, 1, 2]) * 365 / 12
     actual = v2phase(simuated_v, simuated_time_range)
     # desired phase is calculated by hand
     desired = np.array([0.0, 0.1, 0.2]) * 4 * np.pi
@@ -15,35 +15,56 @@ def test_v2phase():
 
 
 def test_h2phase():
-    simulated_h=np.array([10.0,11.0,9.0,8.0,13.0])
-    ture_h=np.array([10]*5)
+    simulated_h = np.array([10.0, 11.0, 9.0, 8.0, 13.0])
+    ture_h = np.array([10]*5)
     # actual=h2phase(simulated_h)
-    actual=h2phase(ture_h)
-    desired=h2phase(ture_h)
-    assert np.isclose(actual,desired).all()
+    actual = h2phase(ture_h)
+    desired = h2phase(ture_h)
+    assert np.isclose(actual, desired).all()
+
+
 def test_generate_phase_noise():
-    noise=generate_phase_noise(0.1)
-    assert noise.shape==(1,20)
+    noise = generate_phase_noise(0.1)
+    assert noise.shape == (1, 20)
+
 
 def test_wrap_phase():
-    v_orig=0.01
-    h_orig=[10.0]*20+np.random.normal(size=(1,20))
-    noise_level=0.1
-    phase_unwrapped=construct_simulated_arc_phase(v_orig,h_orig,noise_level)
-    assert phase_unwrapped.shape==(1,20)
+    v_orig = 0.01
+    h_orig = [10.0]*20+np.random.normal(size=(1, 20))
+    noise_level = 0.1
+    phase_unwrapped = construct_simulated_arc_phase(
+        v_orig, h_orig, noise_level)
+    assert phase_unwrapped.shape == (1, 20)
+
+
 def test_construct_param_search():
-    Bn=np.mat(np.array([1]*20)).T
-    simulated=construct_param_search_space(1,20,Bn)
-    A = np.mat(np.array([np.linspace(2.0,40,20),[1.0]*20]).T)
-    step=[0.001,1]
-    Nsearch=[200,20]
-    Search_space1=np.mat(np.arange(-Nsearch[1]*step[1],Nsearch[1]*step[1],step[1]))
-    actual=np.dot(A[:,1],Search_space1)
-    assert  np.isclose(actual, simulated).all()
+    Bn = np.mat(np.array([1]*20)).T
+    simulated = construct_param_search_space(1, 20, Bn)
+    A = np.mat(np.array([np.linspace(2.0, 40, 20), [1.0]*20]).T)
+    step = [0.001, 1]
+    Nsearch = [200, 20]
+    Search_space1 = np.mat(
+        np.arange(-Nsearch[1]*step[1], Nsearch[1]*step[1], step[1]))
+    actual = np.dot(A[:, 1], Search_space1)
+    assert np.isclose(actual, simulated).all()
+
+
 def test_maximum():
-    dphase=np.mat([[1+1j,1+2j,1+3j]]).T
-    search_space=np.mat([[1j,2j,3j],[1j,2j,3j],[1j,2j,3j]])
-    simulated=maximum_main(dphase,search_space)
-    actual=np.exp(3)
-    assert simulated==actual
-    
+    dphase = np.mat([[-0.5, 0, 0.5]]).T*np.pi
+    search_space = np.mat(
+        [[-0.5, -0.4, -0.3, -0.2, -0.1, 0.1, 0.2, 0.3, 0.4]]*3)*np.pi
+    row_num = 9
+    num_search = [3, 3]
+    parm = [0, 0]
+    Nsearch = [1, 1]
+    step = [1, 2]
+    simulated = maximum_coh_temporal(dphase, search_space,
+                                     row_num, num_search, parm, Nsearch, step)
+    # simulated=[best, parm, a, best_index],a is the index of param_search
+    # assert simulated[0] == 1
+    # assert simulated[1] == [[-1], [-2]]
+    # assert simulated[2] == [[1], [1]]
+    # assert simulated[3] == 1
+    actual = [np.exp(-0.4j*np.pi)+np.exp(0.1j*np.pi)+np.exp(0.6j*np.pi)]
+    assert np.isclose(actual, simulated[0]).all()
+    # assert simulated == actual
