b84e107f5cb2be17583af151b61b8a616fb4064f
applepie1026 <1287499419@qq.com>
2023-05-22 16:01:59 +0800

    function of calculating success rate

--- template/periodogram_lab.py
+++ template/periodogram_lab.py
@@ -5,7 +5,7 @@ import numpy as np
 # ------------------------------------------------
 # initial parameters
 # ------------------------------------------------
-v = np.linspace(0.001, 0.01, 10)
+v = np.linspace(0.01, 0.1, 10)
 # v = 0.05
 h = np.linspace(30, 40, 10)
 # h = 30
@@ -13,7 +13,7 @@ WAVELENGTH = 0.0056  # [unit:m]
 Nifg = 20
 noise_level = 0.0
 noise_phase = af.sim_phase_noise(noise_level, Nifg)
-step_orig = np.array([1.0, 0.0001])
+step_orig = np.array([1.0, 0.001])
 std_param = np.array([40, 0.06])
 param_orig = np.array([0, 0])
 param_name = ["height", "velocity"]
@@ -24,36 +24,36 @@ Num_search2 = af.compute_Nsearch(std_param[1], step_orig[1])
 Num_search = np.array([Num_search1, Num_search2])
 
 # simulate baseline
-# normal_baseline = np.random.normal(size=(1, Nifg)) * 300
+normal_baseline = np.random.normal(size=(1, Nifg)) * 300
 # print(normal_baseline)
 time_baseline = np.arange(1, Nifg + 1, 1).reshape(1, Nifg)  # 减小重访周期 dt 能明显改善结果
-normal_baseline = np.array(
-    [
-        [
-            -235.25094786,
-            -427.79160933,
-            36.37235105,
-            54.3278281,
-            -87.27348344,
-            25.31470275,
-            201.85998322,
-            92.22902115,
-            244.66603228,
-            -89.80792772,
-            12.17022031,
-            -23.71273067,
-            -241.58736045,
-            -184.03477855,
-            -15.97933883,
-            -116.39428378,
-            -545.53546226,
-            -298.89492777,
-            -379.2293736,
-            289.30702061,
-        ]
-    ]
-)
-# calculate the input parameters of phase
+# normal_baseline = np.array(
+#     [
+#         [
+#             -235.25094786,
+#             -427.79160933,
+#             36.37235105,
+#             54.3278281,
+#             -87.27348344,
+#             25.31470275,
+#             201.85998322,
+#             92.22902115,
+#             244.66603228,
+#             -89.80792772,
+#             12.17022031,
+#             -23.71273067,
+#             -241.58736045,
+#             -184.03477855,
+#             -15.97933883,
+#             -116.39428378,
+#             -545.53546226,
+#             -298.89492777,
+#             -379.2293736,
+#             289.30702061,
+#         ]
+#     ]
+# )
+# # calculate the input parameters of phase
 v2ph = af.v_coef(time_baseline).T
 h2ph = af.h_coef(normal_baseline).T
 # print(h2ph)
